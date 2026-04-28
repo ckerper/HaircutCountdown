3930f5e268482e116c11d9d97f9ffd3cbf9bf964
ckerper <clairekerper@gmail.com>
2026-04-28 14:41:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const events = [
     { name: "Buzz", datetime: new Date("2026-03-03T19:00:00") },
     { name: "Haircut", datetime: new Date("2026-03-31T15:00:00") },
     { name: "Buzz", datetime: new Date("2026-05-02T15:45:00") },
-    { name: "Buzz", datetime: new Date("2026-05-26T13:00:00") },
+    { name: "Buzz", datetime: new Date("2026-05-26T17:15:00") },
     { name: "Haircut", datetime: new Date("2026-06-16T13:00:00") },
     { name: "Buzz", datetime: new Date("2026-07-09T14:15:00") }
 ];
